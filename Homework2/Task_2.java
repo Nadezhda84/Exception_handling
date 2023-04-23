9b702c98536f5736eca6b1a97edb8c5f545ebf45
Dusha <nadezdafurman84@gmail.com>
2023-04-23 14:54:25 +0600

    Added solution Task 2

--- Homework2/Task_2.java
+++ Homework2/Task_2.java
@@ -12,5 +12,17 @@
 package Homework2;
 
 public class Task_2 {
+   public static void main(String[] args) {
+      try {
+         int d = 0;
+         int[] intArray = {1, 2, 3, 4, 5, 7, 8, 9, 10};
+         double catchedRes1 = intArray[8] / d;
+         System.out.println("catchedRes1 = " + catchedRes1);
+      } catch (ArithmeticException e) {
+         System.out.println("Деление на ноль!");
+      } catch (IndexOutOfBoundsException ex) {
+         System.out.println("Массив выходит за пределы своего размера!");
+      }
+   }
     
 }
