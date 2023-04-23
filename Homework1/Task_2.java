a2321cc428b2b29bae2adc531ad8a967c7c2667a
Dusha <nadezdafurman84@gmail.com>
2023-04-23 11:47:59 +0600

    Added solution Task 2

--- Homework1/Task_2.java
+++ Homework1/Task_2.java
@@ -4,5 +4,23 @@
 package Homework1;
 
 public class Task_2 {
-    
+    public static void main(String[] args) throws Exception {
+        int[] arr1 = {1, 10, 3};
+        int[] arr2 = {2, 8, 15, 0};
+        diffArray(arr1, arr2);
+        
+    }
+
+    public static void diffArray(int[] arr1, int[] arr2) throws Exception{
+        if (arr1.length !=arr2.length){
+            throw new Exception("Array lengths do not match!");
+        }
+        else{
+            int[] newArr = new int[arr1.length];
+            for(int i=0; i < arr1.length; i++){
+                newArr[i]=arr1[i]-arr2[i];
+                System.out.print(newArr[i]+" ");
+            }
+        }
+    }
 }
